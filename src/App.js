8c9f0c7a66bbbcca33d543053dc8e4b52fd18cbf
Akash52 <ac8572611@gmail.com>
2022-02-05 21:34:41 +0530

    Pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Footer from './Components/Footer'
-import Navbar from './Components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './Components/Layout/Footer'
+import Navbar from './Components/Layout/Navbar'
+import About from './Pages/About'
+import Home from './Pages/Home'
+import NotFound from './Pages/NotFound'
 
 const App = () => {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className="container mx-auto px-3 pb-12">Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notFound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         </div>
         <div className="flex-1 px-2 mx-2">
           <div className="flex justify-end">
-            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
               Home
             </Link>
             <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
