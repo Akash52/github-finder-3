c42aef7803be6b6c16f28ea0f4224349eb6a627a
Akash52 <ac8572611@gmail.com>
2022-02-05 23:33:23 +0530

    Setup Github Context

--- src/App.js
+++ src/App.js
@@ -5,23 +5,26 @@ import Navbar from './Components/Layout/Navbar'
 import About from './Pages/About'
 import Home from './Pages/Home'
 import NotFound from './Pages/NotFound'
+import { GithubProvider } from './context/github/GithubContext'
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notFound" element={<NotFound />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notFound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   )
 }
 
