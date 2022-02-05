ec1b69b1fccf6ab3f786366906e0f3625f130d47
Akash52 <ac8572611@gmail.com>
2022-02-05 22:35:38 +0530

    Loading Spinner

--- src/Components/Users/UserResults.jsx
+++ src/Components/Users/UserResults.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Spinner from '../Layout/Spinner'
 
 const UserResults = () => {
   const [users, setUsers] = useState([])
@@ -28,7 +29,7 @@ const UserResults = () => {
       </div>
     )
   } else {
-    return <h3>Loading...</h3>
+    return <Spinner />
   }
 }
 
